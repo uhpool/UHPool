1be82178a98805b760889050e434df321a6e6899
zac <zknoebel@hawaii.edu>
2017-04-25 15:54:47 -1000

    adding locations

--- app/imports/startup/server/publications.js
+++ app/imports/startup/server/publications.js
@@ -1,5 +1,7 @@
 import { Interests } from '/imports/api/interest/InterestCollection';
 import { Profiles } from '/imports/api/profile/ProfileCollection';
+import { Places } from '/imports/api/place/PlaceCollection';
 
 Interests.publish();
 Profiles.publish();
+Places.publish();
--- app/imports/ui/pages/edit/edit.js
+++ app/imports/ui/pages/edit/edit.js
@@ -1,28 +1,19 @@
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 import { _ } from 'meteor/underscore';
+import { Places } from '/imports/api/places/PlacesCollection';
 import { Profiles } from '/imports/api/profile/ProfileCollection';
-import { places } from '/imports/api/places/PlacesCollection';
-
- const selectedPlacesKey = 'selectedPlaces';
 
 Template.Edit_Page.onCreated(function onCreated() {
   this.subscribe(Places.getPublicationName());
-  this.subscribe(Profiles.getPublicationName());
   this.messageFlags = new ReactiveDict();
   this.messageFlags.set(selectedPlacesKey, undefined);
 });
 
 Template.Edit_Page.helpers({
+
   profiles() {
-    // Initialize selectedInterests to all of them if messageFlags is undefined.
-    // if (!Template.instance().messageFlags.get(selectedInterestsKey)) {
-    //   Template.instance().messageFlags.set(selectedInterestsKey, _.map(Interests.findAll(), interest => interest.name));
-    // }
-    // Find all profiles with the currently selected interests.
-    const allProfiles = Profiles.findAll();
-    // const selectedInterests = Template.instance().messageFlags.get(selectedInterestsKey);
-    // return _.filter(allProfiles, profile => _.intersection(profile.interests, selectedInterests).length > 0);
+     return Profiles.findAll();
   },
 
   places() {
@@ -30,7 +21,6 @@ Template.Edit_Page.helpers({
           function makePlaceObject() {
             return {
               label: place.name,
-              selected: _.contains(Template.instance().messageFlags.get(selectedPlacesKey), place.name),
             }
           })
   },

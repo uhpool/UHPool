764a52b21750921308f969f7b0eb519c43b6a3bd
Joshua Nishiguchi <jsn9@hawaii.edu>
2017-04-27 06:05:34 -1000

    Modificatoins to UserAcceptedListings and added a test

--- app/imports/api/base/BaseUtilities.js
+++ app/imports/api/base/BaseUtilities.js
@@ -1,5 +1,6 @@
 import { Profiles } from '/imports/api/profile/ProfileCollection';
 import { Interests } from '/imports/api/interest/InterestCollection';
+//import { UserAcceptedListings } from '/imports/api/user_accepted_listings/UserAcceptedListingsCollection';
 
 export function removeAllEntities() {
   Profiles.removeAll();
--- app/imports/ui/pages/listing/listings.js
+++ app/imports/ui/pages/listing/listings.js
@@ -1,46 +1,58 @@
-// import { Template } from 'meteor/templating';
-// import { ReactiveDict } from 'meteor/reactive-dict';
-// import { _ } from 'meteor/underscore';
-// import { Profiles } from '/imports/api/profile/ProfileCollection';
-// import { Interests } from '/imports/api/interest/InterestCollection';
-//
-// const selectedInterestsKey = 'selectedInterests';
-//
-// Template.Filter_Page.onCreated(function onCreated() {
-//   this.subscribe(Interests.getPublicationName());
-//   this.subscribe(Profiles.getPublicationName());
-//   this.messageFlags = new ReactiveDict();
-//   this.messageFlags.set(selectedInterestsKey, undefined);
-// });
-//
-// Template.Filter_Page.helpers({
-//   profiles() {
-//     // Initialize selectedInterests to all of them if messageFlags is undefined.
-//     if (!Template.instance().messageFlags.get(selectedInterestsKey)) {
-//       Template.instance().messageFlags.set(selectedInterestsKey, _.map(Interests.findAll(), interest => interest.name));
-//     }
-//     // Find all profiles with the currently selected interests.
-//     const allProfiles = Profiles.findAll();
-//     const selectedInterests = Template.instance().messageFlags.get(selectedInterestsKey);
-//     return _.filter(allProfiles, profile => _.intersection(profile.interests, selectedInterests).length > 0);
-//   },
-//
-//   interests() {
-//     return _.map(Interests.findAll(),
-//         function makeInterestObject(interest) {
-//           return {
-//             label: interest.name,
-//             selected: _.contains(Template.instance().messageFlags.get(selectedInterestsKey), interest.name),
-//           };
-//         });
-//   },
-// });
-//
-// Template.Filter_Page.events({
-//   'submit .filter-data-form'(event, instance) {
-//     event.preventDefault();
-//     const selectedOptions = _.filter(event.target.Interests.selectedOptions, (option) => option.selected);
-//     instance.messageFlags.set(selectedInterestsKey, _.map(selectedOptions, (option) => option.value));
-//   },
-// });
-//
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { _ } from 'meteor/underscore';
+import { AllListings, AllListingsCollection } from '../../../api/all_listings/AllListingsCollection';
+import { Profiles } from '../../../api/profile/ProfileCollection';
+import { UserAcceptedListings, UserAcceptedListingsCollection } from '../../../api/user_accepted_listings/UserAcceptedListingsCollection';
+
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+//Meteor.user().profile.name -- Get username (jsn9)
+
+const displayErrorMessages = 'displayErrorMessages';
+
+Template.Listings_Page.onCreated(function onCreated() {
+    this.subscribe('AllListingsCollection');
+    //this.subscribe(Profiles.getPublicationName());
+    //this.subscribe('UserAcceptedListingsCollection');
+    this.messageFlags = new ReactiveDict();
+    this.messageFlags.set(displayErrorMessages, false);
+    this.context = AllListingsCollection.namedContext('Listings_Page');
+});
+
+Template.Listings_Page.helpers({
+    userPendingListings() {
+        const allThings = AllListings.find().fetch();
+        let returnedThings = {};
+        const user = Meteor.user().profile.name;
+        for(let i = 0; i<allThings.length; i++) {
+            //if true can continue because we aren't going to show the same user and is part of 'pending'
+            if(allThings[i].username != user && allThings[i].statusIndicator == "1") {
+
+            }
+        }
+
+        return returnedThings;
+    },
+    userAllListings() {
+        const allThings = AllListings.find().fetch();
+        let returnedThings = {};
+        const user = Meteor.user().profile.name;
+        for(let i = 0; i<allThings.length; i++) {
+            if(allThings[i].username != user) { //can continue because we aren't going to show the same user
+
+            }
+        }
+
+        return returnedThings;
+    },
+});
+
+Template.Listings_Page.events({
+  'submit .filter-data-form'(event, instance) {
+    event.preventDefault();
+    const selectedOptions = _.filter(event.target.Interests.selectedOptions, (option) => option.selected);
+    instance.messageFlags.set(selectedInterestsKey, _.map(selectedOptions, (option) => option.value));
+  },
+});
+
